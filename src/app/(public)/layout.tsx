c4674625fb466d1f733019506d5c86da5e692e91
Jeremiasj-g <120017819+Jeremiasj-g@users.noreply.github.com>
2026-09-15 12:49:15 -0300

    Aplica Outfit al sitio publico

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,18 @@
+import { Outfit } from 'next/font/google';
+import './public.css';
+import { PublicTypographyPortalSync } from './PublicTypographyPortalSync';
+
+const publicFont = Outfit({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+  display: 'swap',
+});
+
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={`${publicFont.className} public-site`}>
+      <PublicTypographyPortalSync />
+      {children}
+    </div>
+  );
+}
